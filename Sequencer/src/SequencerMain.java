e074e7dd6485bdc65771014ee45ae239ae4f572f
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-03-16 23:37:53 -0400

    Added code to close logger handler .

--- Sequencer/src/SequencerMain.java
+++ Sequencer/src/SequencerMain.java
@@ -3,6 +3,9 @@ import java.io.Serializable;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Logger;
 
 public class SequencerMain extends Thread implements Serializable {
 
@@ -13,10 +16,20 @@ public class SequencerMain extends Thread implements Serializable {
   int replica3Port = 9003;
   int replica4Port = 9004;
   int sequencerPort = 9090;
+  static Logger logger = null;
 
   public static void main(String[] args) {
     SequencerMain sequencerMain = new SequencerMain(); //can not use this. as it is not static
-    sequencerMain.start();
+    try {
+      sequencerMain.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      if (sequencerMain.logger != null) {
+        SequencerUtils.closeLoggerHandlers(sequencerMain.logger);
+      }
+    }
+
   }
 
   @Override
@@ -24,13 +37,18 @@ public class SequencerMain extends Thread implements Serializable {
     byte requestBuffer[] = new byte[1000];
     try {
       sequencerSocket = new DatagramSocket(sequencerPort);
+      logger = SequencerUtils
+          .setupLogger(Logger.getLogger("sequencerLogger"), "sequencer.log", false);
     } catch (SocketException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
       e.printStackTrace();
     }
     while (true) {
       DatagramPacket requestReceived = new DatagramPacket(requestBuffer, requestBuffer.length);
       try {
         System.out.println("Sequencer is listening at " + sequencerPort);
+        logger.info("Sequencer is listening at " + sequencerPort);
         sequencerSocket.receive(requestReceived);
         //once request received , there should be new unique thread to handle the request.
         //Concurrent Sequencer will handle the request
@@ -40,5 +58,7 @@ public class SequencerMain extends Thread implements Serializable {
         e.printStackTrace();
       }
     }
+
   }
+
 }
