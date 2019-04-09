af582e7b83dbc6d5a366befdd6600f1683521bbe
rohitsharma <rohit.sharma06596@gmail.com>
2019-04-08 20:59:19 -0400

    Rohit Final changes

--- RequestHandler/src/ConcurrentRequestHandler.java
+++ RequestHandler/src/ConcurrentRequestHandler.java
@@ -458,6 +458,11 @@ public class ConcurrentRequestHandler extends Thread {
         else if (methodName.equalsIgnoreCase(RequestHandlerConstants.METHOD_SIMULATE_SOFTWARE_BUG)) {
         return appendForSFBug(responseString);
     }
+
+        else if (methodName.equalsIgnoreCase(RequestHandlerConstants.METHOD_SIMULATE_CRASH)) {
+            return appendForCrash(responseString);
+        }
+
         return responseString;
 }
 
